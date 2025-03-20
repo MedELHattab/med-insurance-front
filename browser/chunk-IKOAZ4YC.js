5793ed766256086ebeaeebe898ed01da5c0562ed
MedELHattab <136116042+MedELHattab@users.noreply.github.com>
2025-03-20 15:09:23 +0000

    Deploying to gh-pages from @ MedELHattab/med-insurance-front@e8be89f81a88aeba8c2087aeecbf2f737cc81466 🚀

---
+++ browser/chunk-IKOAZ4YC.js
@@ -0,0 +1,1 @@
+import{lb as t}from"./chunk-IYLSK3UX.js";function n(){let e=localStorage.getItem("token");return new t({"Content-Type":"application/json",Authorization:`Bearer ${e}`})}export{n as a};
